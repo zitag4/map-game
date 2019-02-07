97cc99ac70bf35cdcec82cb4610291a6b890222c
Zita Balint <zitag4@gmail.com>
2019-02-07 17:43:18 +0100

    get lat/long for cities from json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import * as dataLocations from './capitalCities1.json';
 import './App.css';
 
 class App extends Component {
   state= {
+    locations: dataLocations,
     map: '',
     marker: ''
   }
@@ -11,19 +12,24 @@ class App extends Component {
 
 
   componentDidMount() {
-        window.initMap = this.initMap
-       loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDRxmJRw4I4YQIMPSBFVuYfuWl79PLyDZQ&v=3&callback=initMap')
+    window.initMap = this.initMap
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDRxmJRw4I4YQIMPSBFVuYfuWl79PLyDZQ&v=3&callback=initMap')
 
     //    loadScript('https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDRxmJRw4I4YQIMPSBFVuYfuWl79PLyDZQ&center=39.086053069081146,40.55353826195705&zoom=4&format=png&maptype=roadmap&style=feature:administrative%7Cvisibility:off&style=feature:administrative.country%7Cvisibility:on&style=feature:administrative.country%7Celement:labels.text%7Cvisibility:off&style=feature:landscape%7Cvisibility:off&style=feature:poi%7Cvisibility:off&style=feature:road%7Cvisibility:off&style=feature:water%7Cvisibility:off&style=feature:water%7Celement:geometry%7Cvisibility:on&style=feature:water%7Celement:labels.text%7Cvisibility:off&size=480x360');
 
-      }
+    console.log(this.state.locations);
+
+
+
+  }
+
 
 
   initMap = () => {
+
+
     const google = window.google;
     let marker;
-
-
     // Constructor creates a new map
     let map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 52.3664032, lng: 9.7441556},
@@ -105,28 +111,40 @@ class App extends Component {
             ]
           }
         ]
-
     });
 
     this.setState({map: map})
+
     //add Marker on click
-      google.maps.event.addListener(map, 'click', function(event) {
+    google.maps.event.addListener(map, 'click', function(event) {
         var latitude = event.latLng.lat();
-    var longitude = event.latLng.lng();
-    console.log( latitude + ', ' + longitude );
-      marker = new google.maps.Marker({
-        position: {lat: event.latLng.lat(), lng: event.latLng.lng()},
-        map: map
-    });
+        var longitude = event.latLng.lng();
+        console.log( latitude + ', ' + longitude );
+
+        marker = new google.maps.Marker({
+          position: {lat: event.latLng.lat(), lng: event.latLng.lng()},
+          map: map
+        });
+
       //igazitja a terkepet
-    //  map.panTo(new google.maps.LatLng(latitude,longitude));
-    });
+      //  map.panTo(new google.maps.LatLng(latitude,longitude));
+      })
+      this.setState({marker: marker})
+      //search for cities geocode
+      let geocoder = new google.maps.Geocoder();
+      for (let i=0; i<this.state.locations.default.capitalCities.length; i++) {
+          let city = this.state.locations.default.capitalCities[i].capitalCity;
+          geocoder.geocode({'address': city}, (results, status) => {
+            if (status === 'OK') {
+              let lat=results[i].geometry.location.lat();
+              let long=results[i].geometry.location.lng();
+              console.log(lat,long)
+            }
+            else console.log('error')
+          });
+      console.log('MMMM' + city) }
 
-    this.setState({marker: marker})
-
-  }
-
-
+      }
 
   render() {
     return (
